1ea8ace14a1d98d52b1b31862b3587e190d00f73
deepvn98 <deepvn988@gmail.com>
2021-05-05 14:24:06 +0700

    bai tap

--- QuanLyNhaTro/src/controller/ManagerHotel.java
+++ QuanLyNhaTro/src/controller/ManagerHotel.java
@@ -2,17 +2,16 @@ package controller;
 
 import model.Person;
 import model.Room;
+import storage.FileManagerPerson;
 import storage.FileManagerRoom;
-import view.Client;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class ManagerHotel {
     private String name;
-    private ArrayList<Room> rooms;
-    private ArrayList<Person> persons;
+    private ArrayList<Room> rooms = new ArrayList<>();
+    private ArrayList<Person> persons = new ArrayList<>();
 
     public ManagerHotel() {
     }
@@ -33,6 +32,9 @@ public class ManagerHotel {
             if (getRoom() != null) {
                 getRoom().setNumberdayinhotel(dayInHotel);
                 getRoom().setPerson(getPersonByID(id));
+                persons.remove(getPersonByID(id));
+                FileManagerPerson fileManagerPerson = FileManagerPerson.getINSTANCE("Sáng");
+                fileManagerPerson.writeFile(persons);
                 FileManagerRoom fileManagerRoom = FileManagerRoom.getINSTANCE("Sáng");
                 fileManagerRoom.writeFile(rooms);
             }
@@ -41,9 +43,21 @@ public class ManagerHotel {
     }
 
     //    thêm người vào mảng người
-    public ArrayList<Person> addPerson(Person person) {
+    public ArrayList<Person> addPerson(Person person) throws IOException {
         persons.add(person);
+        FileManagerPerson fileManagerPerson = FileManagerPerson.getINSTANCE("Sáng");
+        fileManagerPerson.writeFile(persons);
         return persons;
+    }
+//    Tìm người trong mảng người
+    public boolean findPersonInPersons(String id){
+        boolean check = false;
+        for (int i = 0; i< persons.size();i++){
+            if (persons.get(i).getId().equalsIgnoreCase(id)){
+                check = true;
+                return check;
+            }
+        }return check;
     }
 
     //    Tìm người trong mảng người bằng id
@@ -59,8 +73,10 @@ public class ManagerHotel {
     }
 
     //    Thêm phòng vào mảng phòng
-    public ArrayList<Room> addRoom(Room r) {
+    public ArrayList<Room> addRoom(Room r) throws IOException {
         rooms.add(r);
+        FileManagerRoom fileManagerRoom = FileManagerRoom.getINSTANCE("Sáng");
+        fileManagerRoom.writeFile(rooms);
         return rooms;
     }
 
@@ -72,7 +88,71 @@ public class ManagerHotel {
                 room = rooms.get(i);
                 return room;
             }
-        } return null;
+        }
+        return null;
+    }
+
+    //    Xoá người trong phòng
+    public void removePersonAtRoom(String id) throws IOException {
+        for (int i = 0; i < rooms.size(); i++) {
+            if (rooms.get(i).getPerson()!=null){
+                if (rooms.get(i).getPerson().getId().equals(id)) {
+                    rooms.get(i).setNumberdayinhotel(0);
+                    rooms.get(i).setPerson(null);
+                    FileManagerRoom fileManagerRoom = FileManagerRoom.getINSTANCE("Sáng");
+                    fileManagerRoom.writeFile(rooms);
+                }
+            }else {
+                System.out.println("không có người này trong khách sạn");
+            }
+
+        }
+    }
+
+    //    Tính tiền khách ở trọ
+    public void getAmountMoney(String id) throws IOException {
+        double money = 0;
+        for (int i = 0; i < rooms.size(); i++) {
+            if (rooms.get(i).getPerson()!=null){
+                if (rooms.get(i).getPerson().getId().equalsIgnoreCase(id)) {
+                    money = rooms.get(i).getAmountMoney();
+                    rooms.get(i).setPerson(null);
+                    rooms.get(i).setNumberdayinhotel(0);
+                    FileManagerRoom fileManagerRoom = FileManagerRoom.getINSTANCE("Sáng");
+                    fileManagerRoom.writeFile(rooms);
+                    System.out.println("Số tiền phải thanh toán là:" + money);
+                    break;
+                }
+            }
+        }
+    }
+//   Kiểm tra có người trong phòng không
+    public boolean getPersonAtRoom(String id) {
+        boolean check = false;
+        for (int i = 0; i < rooms.size(); i++) {
+            if (rooms.get(i).getPerson()!=null){
+                if (rooms.get(i).getPerson().getId().equals(id)) {
+                    check = true;
+                    return check;
+                }
+            }
+        }return check;
+    }
+//    sửa thông tin người trong phòng
+    public void editPersonAtRoom(String id,Person person,int numberInRoom){
+        for (int i = 0; i< rooms.size();i++){
+            if (rooms.get(i).getPerson().getId().equals(id)){
+                rooms.get(i).setPerson(person);
+                rooms.get(i).setNumberdayinhotel(numberInRoom);
+                FileManagerRoom fileManagerRoom = FileManagerRoom.getINSTANCE("Sáng");
+                try {
+                    fileManagerRoom.writeFile(rooms);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                break;
+            }
+        }
     }
 
     public String getName() {
@@ -98,4 +178,23 @@ public class ManagerHotel {
     public void setPersons(ArrayList<Person> persons) {
         this.persons = persons;
     }
+    public void showPerson(){
+        if (persons.size()<=0){
+            System.out.println("Hiện tại không có người đăng ký thuê phòng");
+        }
+        if (persons.size()>0){
+            for (Person p:persons
+            ) {
+                System.out.println(p.toString());
+            }
+        }
+
+    }
+    public void showRoom(){
+        for (Room r:rooms
+        ) {
+            System.out.println(r.toString());
+        }
+    }
+
 }
--- QuanLyNhaTro/src/view/Client.java
+++ QuanLyNhaTro/src/view/Client.java
@@ -32,10 +32,11 @@ public class Client {
         }
         ManagerHotel hotel = new ManagerHotel("Sáng", rooms, persons);
 
+
 //        Thêm phòng và người thêu vào dánh sách
-        menuAddPersonAndRoom(hotel,fileManagerRoom,fileManagerPerson,persons,rooms);
+        menuAddPersonAndRoom(hotel, fileManagerRoom, fileManagerPerson, persons, rooms);
 //        Gọi phương thức thêm người vào phòng
-        menuhotelManager(hotel,rooms);
+        menuhotelManager(hotel, rooms);
 //        for (Room r : rooms
 //        ) {
 //            System.out.println(r);
@@ -51,7 +52,9 @@ public class Client {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Nhập 1: Để thêm người muốn thuê trọ:");
         System.out.println("Nhập 2: Để thêm mới phòng trọ:");
-        System.out.println("Nhập 3: Để thoát menu:");
+        System.out.println("Nhập 3: Để xem thông tin phòng trọ:");
+        System.out.println("Nhập 4: Để xem thông tin khách đăng ký thuê phòng trọ:");
+        System.out.println("Nhập 5: Để thoát menu:");
 
         int number = 0;
         boolean check = true;
@@ -67,7 +70,11 @@ public class Client {
                     for (int i = 0; i < arr.length; i++) {
                         System.out.print("Người thứ " + (i + 1));
                         System.out.println();
-                        hotel.addPerson(nhapTTPerson());
+                        try {
+                            hotel.addPerson(nhapTTPerson());
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
                         try {
                             fileManagerPerson.writeFile(persons);
                         } catch (IOException e) {
@@ -84,7 +91,11 @@ public class Client {
                     for (int i = 0; i < arr.length; i++) {
                         System.out.print("Phòng thứ " + (i + 1));
                         System.out.println();
-                        hotel.addRoom(nhapTTRoom());
+                        try {
+                            hotel.addRoom(nhapTTRoom());
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
                         try {
                             fileManagerRoom.writeFile(rooms);
                         } catch (IOException e) {
@@ -92,26 +103,34 @@ public class Client {
                         }
                     }
                     break;
-
                 }
-                case 3: {
+                case 3:
+                {
+                    hotel.showRoom();
+                    break;
+                }
+                case 4:
+                {
+                    hotel.showPerson();
+                    break;
+                }
+                case 5: {
                     check = false;
                     break;
                 }
             }
         } while (check);
-
     }
 
     //    Menu
-    public static void menuhotelManager(ManagerHotel hotel,ArrayList<Room>rooms) {
+    public static void menuhotelManager(ManagerHotel hotel, ArrayList<Room> rooms) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Nhấn 1: thêm người vào phòng");
-        System.out.println("Nhấn 2: ");
+        System.out.println("Nhấn 2: sửa thông tin khách hàng ");
         System.out.println("Nhấn 3: Xoá thông tin khách trọ ");
         System.out.println("Nhấn 4: hiển thị thông tin khách trọ:");
         System.out.println("Nhấn 5: tính tiền khách phải trả:");
-        System.out.println("Nhấn 5: Thoát menu:");
+        System.out.println("Nhấn 6: Thoát menu:");
         boolean check = true;
         do {
             System.out.print("Nhập sự lựa chọn:");
@@ -121,20 +140,45 @@ public class Client {
                     Scanner scanner1 = new Scanner(System.in);
                     System.out.print("Nhập id khách hàng:");
                     String id = scanner1.nextLine();
-                    System.out.print("Số ngày khách muốn ở: ");
-                    Scanner scanner2 = new Scanner(System.in);
-                    int dayInHotel = scanner2.nextInt();
-                    try {
-                        hotel.addPersonAtRoom(id, dayInHotel);
-                    } catch (IOException e) {
-                        e.printStackTrace();
+                    if (hotel.findPersonInPersons(id)){
+                        System.out.print("Số ngày khách muốn ở: ");
+                        Scanner scanner2 = new Scanner(System.in);
+                        int dayInHotel = scanner2.nextInt();
+                        try {
+                            hotel.addPersonAtRoom(id, dayInHotel);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
                     }
                     break;
                 }
                 case 2: {
+                    Scanner scanner1 =new Scanner(System.in);
+                    System.out.print("nhập id khách hàng sửa thông tin");
+                    String id =scanner1.nextLine();
+                    hotel.getPersonAtRoom(id);
+                    if (hotel.getPersonAtRoom(id)){
+                        System.out.print("Số ngày khách muốn ở");
+                        int number = new Scanner(System.in).nextInt();
+                        hotel.editPersonAtRoom(id,nhapTTPerson(),number);
+                    }else {
+                        System.out.println("người dùng k tồn tại");
+                    }
                     break;
                 }
                 case 3: {
+                    System.out.print("Nhập vào id của người bạn muốn xoá:");
+                    Scanner scanner1 = new Scanner(System.in);
+                    String id = scanner1.nextLine();
+                    if (hotel.getPersonAtRoom(id)){
+                        try {
+                            hotel.removePersonAtRoom(id);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }else {
+                        System.err.print("Người này không có trong khách sạn");
+                    }
                     break;
                 }
                 case 4: {
@@ -145,6 +189,17 @@ public class Client {
                     break;
                 }
                 case 5: {
+                    System.out.print("Nhập vào id số chứng minh thư của người thanh toán:");
+                    Scanner scanner1 = new Scanner(System.in);
+                    String id = scanner1.nextLine();
+                    try {
+                        hotel.getAmountMoney(id);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                }
+                case 6: {
                     check = false;
                     break;
                 }
